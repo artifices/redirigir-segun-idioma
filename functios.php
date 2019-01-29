04dda9ed53500c2205321d289316dbf4c50e2f4d
Artifices.net <info@artifices.net>
2019-01-29 15:58:28 +0100

    Update functios.php

--- functios.php
+++ functios.php
@@ -1,6 +1,7 @@
 // CAMBIAR DE PAGINA CADA X SEGUNDOS
 
-
+// if( wp_is_mobile()
+// probar para comprobar si no es movil y de un idioma en concreto, para el caso de las TV
 	
 if (ICL_LANGUAGE_CODE == "es") { 
     //mostrar etiqueta meta en espanol, y que redirija al siguiente idioma
